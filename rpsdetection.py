891a7a2317f6849ffc16a97162c0d8168df6c79a
Piratelle <eratelle@berkeley.edu>
2024-05-10 23:45:45 -0700

    RPSImage class.

--- rpsdetection.py
+++ rpsdetection.py
@@ -1,3 +1,4 @@
+import math
 import os
 import re
 
@@ -53,8 +54,44 @@ class RPSImage:
         # process the image through MMPose, saving output
         result_generator = inferencer(img_path, out_dir=out_path)
         result = next(result_generator)
+        data_dict = result['predictions'][0][0]
         
         # translate results into features
+        ## bounding box ratio: y-dim / x-dim of bounding box
+        bbox = data_dict['bbox'][0]
+        self.box_ratio = (bbox[3] - bbox[1]) / (bbox[2] - bbox[0])
+        
+        ## keypoints ratios[0-5]: y-dim / x-dim of keypoint range, for thumb - pinky and overall
+        kp_range = [[1,4],[5,8],[9,12],[13,16],[17,20],[0,20]]
+        range_count = len(kp_range)
+        min_x = [math.inf for i in range(range_count)]
+        min_y = [math.inf for i in range(range_count)]
+        max_x = [0 for i in range(range_count)]
+        max_y = [0 for i in range(range_count)]
+        self.key_ratio = [0 for i in range(range_count)]
+        keypoints = data_dict['keypoints']
+        for j in range(range_count):
+            min_val, max_val = kp_range[j]
+            for i in range(len(keypoints)):
+                if min_val <= i <= max_val:
+                    x, y = keypoints[i]
+                    if x < min_x[j]: min_x[j] = x
+                    if x > max_x[j]: max_x[j] = x
+                    if y < min_y[j]: min_y[j] = y
+                    if y > max_y[j]: max_y[j] = y
+            if (max_x[j] != min_x[j]): # prevent divide-by-zero errors
+                self.key_ratio[j] = (max_y[j] - min_y[j]) / (max_x[j] - min_x[j])
+    
+    def get_features(self):
+        features = [ self.box_ratio ]
+        for r in self.key_ratio:
+            features.append(r)
+        return features
+    
+    def compare_features(self, other):
+        my_features = self.get_features()
+        ot_features = other.get_features()
+        # more to do here
 
 
 ###############
